b6c128f57f2c294bf8b1f20e4d4f90e43a15f133
p0keT <p0keT@ukr.net>
2019-03-28 16:55:41 +0200

    Decorator pattern

---
+++ src/main/java/patterns/vladimir/DecoratorApp.java
@@ -0,0 +1,80 @@
+package patterns.vladimir;
+
+public class DecoratorApp {
+    public static void main(String[] args) {
+        PrinterInterface printerInterface =
+                //new Printer("Yo");
+                //new LeftBracketDecorator(new Printer("Yo"));
+                //new LeftBracketDecorator(new RightBracketDecorator(new Printer("Yo")));
+                new QuotesDecorator(new LeftBracketDecorator(new RightBracketDecorator(new Printer("Yo"))));
+
+        printerInterface.print();
+    }
+}
+
+interface PrinterInterface{
+    void print();
+}
+
+class Printer implements PrinterInterface{
+
+    String value;
+
+    public Printer(String value) {
+        this.value = value;
+    }
+
+    public void print() {
+        System.out.print(value);
+    }
+}
+
+abstract class Decorator implements PrinterInterface{
+    PrinterInterface component;
+
+    public Decorator(PrinterInterface component) {
+        this.component = component;
+    }
+}
+
+class QuotesDecorator extends Decorator{
+
+    public QuotesDecorator(PrinterInterface component) {
+        super(component);
+    }
+
+    public void print() {
+        System.out.print("\"");
+        component.print();
+        System.out.print("\"");
+
+    }
+}
+
+class LeftBracketDecorator extends Decorator{
+
+
+    public LeftBracketDecorator(PrinterInterface component) {
+        super(component);
+    }
+
+    public void print() {
+        System.out.print("[");
+        component.print();
+    }
+}
+
+
+class RightBracketDecorator extends Decorator{
+
+
+    public RightBracketDecorator(PrinterInterface component) {
+        super(component);
+    }
+
+    public void print() {
+        component.print();
+        System.out.print("]");
+
+    }
+}
